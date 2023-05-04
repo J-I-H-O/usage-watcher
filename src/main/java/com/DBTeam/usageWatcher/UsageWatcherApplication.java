514340a4d2bafd3738d0101fb09c0ae027c49825
J-I-H-O <jeongjiho0731@gmail.com>
2023-05-04 21:20:38 +0900

    init

---
+++ src/main/java/com/DBTeam/usageWatcher/UsageWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.DBTeam.usageWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DBTeam/usageWatcher/UsageWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBTeam.usageWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
